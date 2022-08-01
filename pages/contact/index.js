d1deb2c162a06f5fd0b891e9bf9006c516f7cf87
Nidja2021 <pipo.m.ajdin@gmail.com>
2022-08-02 00:23:38 +0200

    second commit

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -13,7 +13,7 @@ export default function Contact() {
       <form className={styles.contact__form}>
           <div className={styles.contact__form__text}>
             <p>You have interesting project for me ?</p>
-            <p>Okay, send me message and let's talk :)</p>
+            <p>Okay, send me message and lets talk :)</p>
           </div>
           <div className={styles.contact__form__inputControls}>
             <input className={styles.contact__form__controls} type="text" placeholder='Name...' />
