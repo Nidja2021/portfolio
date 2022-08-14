d946b92ff2f707319db7643b21de7a0b9b9409a7
Nidja2021 <pipo.m.ajdin@gmail.com>
2022-08-14 23:56:01 +0200

    new changes

---
+++ pages/api/email.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+import nodemailer from 'nodemailer'
+
+export default async function sendEmail(req, res) {
+
+    const { name, email, subject, message } = req.body;
+
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.USER,
+        pass: process.env.PASS,
+      },
+    });
+    try {
+      const emailRes = await transporter.sendMail({
+        from: email,
+        to: 'pipo.m.ajdin@gmail.com',
+        subject: subject,
+        html: `<p>Email sent from ${name}</p>
+          <p>Email: ${email}</p>
+          <p>Subject: ${subject}</p>
+          <p>Message: ${message}</p>
+        `
+      })
+      
+    } catch (error) {
+      return res.status(200).json({message: error.message})
+    }
+    return res.status(200).json({message: 'Email sent successfully'})
+}
+
--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,30 +1,100 @@
+import { useRef, useState } from 'react';
+import axios from 'axios';
+
 import styles from '../../styles/contact.module.scss';
 
 export default function Contact() {
+  const [inputs, setInputs] = useState({
+    name:'',
+    email:'',
+    subject:'',
+    message:''
+  })
+
+  const formRef = useRef()
+
+  const handleChange = (e) => {
+    setInputs({ ...inputs, [e.target.name]: e.target.value });  
+  }
+
+  async function onSubmitEmail(e) {
+    e.preventDefault()
+    let config = {
+      method: 'post',
+      url: `/api/email`,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: inputs,
+    };
+    try {
+      const response = await axios(config);
+      console.log(response);
+      if (response.status == 200) {
+        // reset();
+        // toast(
+        //   'success',
+        //   'Thank you for contacting us, we will be in touch soon.'
+        // );
+        console.log('success');
+      }
+    } catch (error) {
+      console.log('error', error.message);
+    }
+  }
+
   return (
     <section className={styles.contact}>
 
       <h1 className={styles.contact__title}>Contact</h1>
 
-      <form className={styles.contact__form}>
+      <div className={styles.contact__form}>
           <div className={styles.contact__form__text}>
             <p>You have interesting project for me ?</p>
             <p>I would like to hear more about it, send me message and let's build something incredible :)</p>
           </div>
-          <div className={styles.contact__form__inputControls}>
-            <input className={styles.contact__form__controls} type="text" placeholder='Name...' />
-            <input className={styles.contact__form__controls} type="text" placeholder='Email...'/>
-            <input className={styles.contact__form__controls} type="text" placeholder='Subject...'/>
-            <textarea className={`${styles.contact__form__controls} ${styles.contact__form__textarea}`} placeholder='Message...'></textarea>
+          
+          <form onSubmit={onSubmitEmail} className={styles.contact__form__inputControls}>
+            <input 
+              type="text" 
+              placeholder='Name...' 
+              name='name' 
+              className={styles.contact__form__controls}
+              value={inputs.name}
+              onChange={handleChange}
+            />
+            <input 
+              type="text" 
+              placeholder='Email...' 
+              name='email' 
+              className={styles.contact__form__controls}
+              value={inputs.email}
+              onChange={handleChange}
+            />
+            <input 
+              type="text" 
+              placeholder='Subject...' 
+              name='subject' 
+              className={styles.contact__form__controls}
+              value={inputs.subject}
+              onChange={handleChange}
+            />
+            <textarea 
+              placeholder='Message...' 
+              name='message' 
+              className={`${styles.contact__form__controls} ${styles.contact__form__textarea}`}
+              value={inputs.message}
+              onChange={handleChange}
+            ></textarea>
             <button className={`${styles.contact__form__controls} ${styles.contact__form__btn}`}>Send Message</button>
-          </div>
+          </form>
           
           <div className={styles.contact__form__networks}>
             <a href="#" className={styles.contact__form__networks__links}>github</a>
             <a href="#" className={styles.contact__form__networks__links}>linkedin</a>
             <a href="#" className={styles.contact__form__networks__links}>codepen</a>
           </div>
-        </form>
+        </div>
       
     </section>
   )
