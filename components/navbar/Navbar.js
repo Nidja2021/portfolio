23766c041a122c2a411f9a98cc5d5f7d13931787
Nidja2021 <pipo.m.ajdin@gmail.com>
2023-04-24 15:58:23 +0200

    updating about me and pdf

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -6,48 +6,48 @@ import Image from "next/image"
 export default function Navbar() {
 
     const router = useRouter()
-    
+
     const handleHomeImage = () => {
         router.push('/')
     }
-  return (
-    <nav className={styles.navbar}>
-    
-       <div className={`${styles.navbar__menu}`}>
+    return (
+        <nav className={styles.navbar}>
 
-       <div className={styles.navbar__menu__logo}>
-        <div className={styles.navbar__menu__logo__image} onClick={handleHomeImage}>
-            <Image src="/assets/logo.png" alt="" width={300} height={300}/>
-        </div>
-        <div className={styles.navbar__menu__logo__text}>
-            <p>Ajdin Pipo</p>
-            <p>Software Developer</p>
-        </div>
-        
-        </div>
-    <div className={styles.navbar__menu__links}>
-        <Link href='/'>
-            <a className={styles.navbar__menu__links__link}>home</a>
-        </Link>
-        <Link href='/about'>
-            <a className={styles.navbar__menu__links__link}>about</a>
-        </Link>
-        <Link href='/skills'>
-            <a className={styles.navbar__menu__links__link}>skills</a>
-        </Link>
-        <Link href='/projects'>
-            <a className={styles.navbar__menu__links__link}>projects</a>
-        </Link>
-        <Link href='/contact'>
-            <a className={styles.navbar__menu__links__link}>contact</a>
-        </Link>
-    </div> 
-    <div className={styles.navbar__menu__networks}>
-            <Link href="https://github.com/Nidja2021"><a className={styles.navbar__menu__networks__network} target='_blank'>GITHUB</a></Link>
-            <Link href="https://www.linkedin.com/in/ajdin-pipo-68b922118/"><a className={styles.navbar__menu__networks__network} target='_blank'>LINKEDIN</a></Link>
-            <Link href="https://codepen.io/Ajdin123"><a className={styles.navbar__menu__networks__network} target='_blank'>CODEPEN</a></Link>
-        </div>       
-       </div>
-    </nav>
-  )
+            <div className={`${styles.navbar__menu}`}>
+
+                <div className={styles.navbar__menu__logo}>
+                    <div className={styles.navbar__menu__logo__image} onClick={handleHomeImage}>
+                        <Image src="/assets/logo.png" alt="" width={300} height={300} />
+                    </div>
+                    <div className={styles.navbar__menu__logo__text}>
+                        <p>Ajdin Pipo</p>
+                        <p>Software Developer</p>
+                    </div>
+
+                </div>
+                <div className={styles.navbar__menu__links}>
+                    <Link href='/'>
+                        <a className={styles.navbar__menu__links__link}>home</a>
+                    </Link>
+                    <Link href='/about'>
+                        <a className={styles.navbar__menu__links__link}>about me</a>
+                    </Link>
+                    <Link href='/skills'>
+                        <a className={styles.navbar__menu__links__link}>skills</a>
+                    </Link>
+                    <Link href='/projects'>
+                        <a className={styles.navbar__menu__links__link}>projects</a>
+                    </Link>
+                    <Link href='/contact'>
+                        <a className={styles.navbar__menu__links__link}>contact</a>
+                    </Link>
+                </div>
+                <div className={styles.navbar__menu__networks}>
+                    <Link href="https://github.com/Nidja2021"><a className={styles.navbar__menu__networks__network} target='_blank'>GITHUB</a></Link>
+                    <Link href="https://www.linkedin.com/in/ajdin-pipo-68b922118/"><a className={styles.navbar__menu__networks__network} target='_blank'>LINKEDIN</a></Link>
+                    <Link href="https://codepen.io/Ajdin123"><a className={styles.navbar__menu__networks__network} target='_blank'>CODEPEN</a></Link>
+                </div>
+            </div>
+        </nav>
+    )
 }
--- pages/about/index.js
+++ pages/about/index.js
@@ -11,33 +11,33 @@ export default function About() {
   return (
     <div className={styles.about}>
 
-    <h1 className={styles.about__title}>about me</h1>
-    
-    <div className={styles.about__aboutMe}>
-      <div className={styles.about__aboutMe__image}>
-        <Image 
-          src='/assets/mypic/me_avatar.png'
-          alt='My Picture'
-          layout='fill'
-          className={styles.about__aboutMe__image__img}
-        />
-      </div>
-      <div className={styles.about__aboutMe__description}>
-        <p>Hello, I'm Full Stack Developer from Bosnia and Herzegovina. I have passion for creating web applications, mostly based on backend development, but also have experience with frontend development, and little experience with UX/UI design. Also I'm bachelor of Mechanical Engineering.</p>
-        <p>I'm ambitious and hardworking individual, problem solver, well-organized person with a good sense of humour, but I take my work seriously.</p>
-        <p>Big fan of NBA, love play a basketball. Also like outdoor activities, such as walking, runing, and hangout with my family and friends.</p>
-        <p>I like to meet new people and work with someone who is optimistic with good energy.</p>
+      <h1 className={styles.about__title}>about me</h1>
 
-        <div className={styles.about__aboutMe__description__resume}>
-          <p>Want to know more about me?</p>
-          <p>You can download my resume.</p>
-          <button className={styles.about__aboutMe__description__resume__btn} onClick={saveFile}>download resume</button>
+      <div className={styles.about__aboutMe}>
+        <div className={styles.about__aboutMe__image}>
+          <Image
+            src='/assets/mypic/me_avatar.png'
+            alt='My Picture'
+            layout='fill'
+            className={styles.about__aboutMe__image__img}
+          />
+        </div>
+        <div className={styles.about__aboutMe__description}>
+          <p>Hello, I am a Full Stack Developer with a passion for creating web applications. My expertise lies mainly in backend development, but I also have experience in frontend development and some knowledge in UX/UI design. Additionally, I hold a Bachelor's degree in Mechanical Engineering and I am currently pursuing a degree in Information Technology.</p>
+          <p>As an ambitious and hardworking individual, I enjoy solving problems and am well-organized, with a good sense of humor, although I take my work very seriously.</p>
+          <p>In my free time, I'm a big fan of the NBA and love playing basketball. I also enjoy outdoor activities, such as walking, running, and hanging out with my family and friends.</p>
+          <p>I like to meet new people and work with someone who is optimistic with good energy.</p>
+
+          <div className={styles.about__aboutMe__description__resume}>
+            <p>Want to know more about me?</p>
+            <p>You can download my resume.</p>
+            <button className={styles.about__aboutMe__description__resume__btn} onClick={saveFile}>download resume</button>
+          </div>
         </div>
       </div>
-    </div>
-    
-    
-        
+
+
+
     </div>
   )
 }
